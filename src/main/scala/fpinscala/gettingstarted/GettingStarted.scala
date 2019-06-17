e9ee3406288a4ef4636f61f04697f47d9e669931
Arnold Farkas <farkasarnold1981@gmail.com>
2019-06-17 22:15:46 +0200

    Getting Started Solutions

--- src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -30,7 +30,15 @@ object MyModule {
 
   // Exercise 1: Write a function to compute the nth fibonacci number
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+
+    @tailrec
+    def go(i0: Int, i1: Int, iterator: Int): Int =
+      if (iterator == 0) i0
+      else go(i1, i0 + i1, iterator - 1)
+
+    go(0, 1, n)
+  }
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) = {
@@ -134,7 +142,12 @@ object PolymorphicFunctions {
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  @tailrec
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean =
+    if (as.size > 1)
+      if (gt(as(0), as(1))) isSorted(as.tail, gt)
+      else false
+    else true
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
@@ -146,12 +159,12 @@ object PolymorphicFunctions {
 
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
-  def curry[A,B,C](f: (A, B) => C): A => (B => C) = ???
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = a => b => f(a, b)
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
-  def uncurry[A,B,C](f: A => B => C): (A, B) => C = ???
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = (a, b) => f(a)(b)
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -164,5 +177,5 @@ object PolymorphicFunctions {
 
   // Exercise 5: Implement `compose`
 
-  def compose[A,B,C](f: B => C, g: A => B): A => C = ???
+  def compose[A,B,C](f: B => C, g: A => B): A => C = a => f(g(a))
 }
